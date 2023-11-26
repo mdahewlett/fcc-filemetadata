8c94f8eee32b4e6d14dd1082619e417305bd4d4b
mdah <courseteam@Courses-MacBook-Pro.local>
2023-11-26 07:15:19 -0800

    chore: copy completed project from replit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+// installed and added multer for single file upload handling
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// passed multer function 'upload' as middleware
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  fileObj = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
+  res.json(fileObj);
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, function() {
+  console.log('Your app is listening on port ' + port);
 });
